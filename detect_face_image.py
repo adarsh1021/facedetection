4e4f8e9fb0a71560d023dc4820948bf278078380
Adarsh Menon <adarsh1021@gmail.com>
2019-04-21 22:27:40 +0530

    add all files

---
+++ detect_face_image.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read the input image
+img = cv2.imread('test.jpg')
+
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
